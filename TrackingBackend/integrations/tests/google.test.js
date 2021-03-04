c933cb376ef4ac58ccd0fb003e7bfcd781a77292
Dylan Mathews <Dylanmathews@gmail.com>
2021-03-04 00:41:13 -0700

    Added further testing with some test stubbing.

--- TrackingBackend/integrations/tests/google.test.js
+++ TrackingBackend/integrations/tests/google.test.js
@@ -1,8 +1,17 @@
 const googleAPI = require("../google")
 
-test("Can grab a list of courses", async () => {
+test("Can get a list of courses", async () => {
 	await googleAPI.build()
 	const courses = await googleAPI.getCourses()
 
 	expect(courses.length).toBeGreaterThanOrEqual(2)
+})
+
+test("Can get a list of assignments for a course", async () => {
+	//TODO: Implement
+})
+
+test("Can get a course by its name", async () => {
+	const course = await googleAPI.getCourseByName("Web Programming")
+	expect(course.name).toBe("Web Programming")
 })
